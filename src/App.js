70a1f8f89debb7eb4bfd3f9ee2ea51a828d2bc24
Achmad Ali B <achmadali.baidlowi@gmail.com>
2020-01-15 23:12:28 +0700

    add shop collection

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Homepage from './pages/Homepage/homepage.component';
-
-const HatsPage = () => (
-  <h1> Its Hats Page</h1>
-);
-const JacketsPage = () => (
-  <h1> Its Jackets Page</h1>
-);
-const SneakersPage = () => (
-  <h1> Its Sneakers Page</h1>
-);
-const WomensPage = () => (
-  <h1> Its Womens Page</h1>
-);
-
-const MensPage = () => (
-  <h1> Its Mens Page</h1>
-);
+import Shop from './pages/Shop/shop.component';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route path='/shop/hats' component={HatsPage} />
-        <Route path='/shop/jackets' component={JacketsPage} />
-        <Route path='/shop/sneakers' component={SneakersPage} />
-        <Route path='/shop/womens' component={WomensPage} />
-        <Route path='/shop/mens' component={MensPage} />
+        <Route path='/shop' component={Shop} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/components/Collections-preview/collections-preview.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import CollectionItem from '../Collection-item/collection-item.component';
+
+const CollectionsPreview = ({title, items}) => {
+  return(
+    <div className="collection-preview">
+      <h2 className="title"> {title} </h2>
+      <div className="preview">
+        {
+          items
+            .filter((item, index) => index < 4)
+            .map(({id, ...itemAttribute}) => <CollectionItem key={id} {...itemAttribute} /> )
+        }
+      </div>
+    </div>
+  );
+}
+
+export default CollectionsPreview;
