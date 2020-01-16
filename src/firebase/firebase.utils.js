a21c1d9375566269543dfb73128c46df07a313f3
Achmad Ali B <achmadali.baidlowi@gmail.com>
2020-01-16 15:18:35 +0700

    add sign in with google feature

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import Config from './firebase.config'
+
+
+firebase.initializeApp(Config)
+
+const auth          = firebase.auth()
+// const firestore     = firebase.firestore()
+const authProvider  = new firebase.auth.GoogleAuthProvider().setCustomParameters({prompt: 'select_account'})
+
+export const  SignInWithGoogle = () => auth.signInWithPopup(authProvider)
+export default firebase
